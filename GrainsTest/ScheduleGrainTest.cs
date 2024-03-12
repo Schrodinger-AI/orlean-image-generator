75f0dab8a4844f085bc287b1c4694fd3dca0fcff
argfoo-nft <argfoo@schrodingernft.ai>
2024-03-12 08:56:01 +0800

    test(scheduler): added unit test for failing request
    Signed-off-by: argfoo-nft <argfoo@schrodingernft.ai>

--- GrainsTest/ScheduleGrainTest.cs
+++ GrainsTest/ScheduleGrainTest.cs
@@ -28,7 +28,7 @@ public class ScheduleGrainTest
         state.State.StartedImageGenerationRequests = new Dictionary<string, RequestAccountUsageInfo>();
 
         // create a new grain - we dont mock the grain here because we do not need to override any base methods
-        var grain = new SchedulerGrain(state, new FakeTimerRegistry(), Mock.Of<ILogger<SchedulerGrain>>());
+        var grain = new SchedulerGrain(state, Mock.Of<ILogger<SchedulerGrain>>());
 
         // set a new value
         var now = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
@@ -38,49 +38,10 @@ public class ScheduleGrainTest
         Mock.Get(state).Verify(_ => _.WriteStateAsync());
         Assert.That(state.State.StartedImageGenerationRequests, Has.Count.EqualTo(1));
     }
-    
+
     [Test]
     public async Task Scheduler_Test()
     {
-        // mock a persistent state item
-        var state = Mock.Of<IPersistentState<SchedulerState>>(_ => _.State == Mock.Of<SchedulerState>());
-        state.State.StartedImageGenerationRequests = new Dictionary<string, RequestAccountUsageInfo>();
-        state.State.FailedImageGenerationRequests = new Dictionary<string, RequestAccountUsageInfo>();
-        state.State.CompletedImageGenerationRequests = new Dictionary<string, RequestAccountUsageInfo>();
-        state.State.PendingImageGenerationRequests = new Dictionary<string, RequestAccountUsageInfo>();
-        state.State.ApiAccountInfoList = new List<APIAccountInfo> { new APIAccountInfo { ApiKey = "ApiKey1", MaxQuota = 5 }, new APIAccountInfo { ApiKey = "ApiKey2", MaxQuota = 5 } };
-
-        var mockTimerRegistry = new FakeTimerRegistry();
-        // create a new grain - we dont mock the grain here because we do not need to override any base methods
-        var grain = new SchedulerGrain(state, mockTimerRegistry, Mock.Of<ILogger<SchedulerGrain>>());
-
-        // set a new value
-        var now = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
-        await grain.AddImageGenerationRequest("myRequestId", "myAccountInfo", now);
-
-        while (mockTimerRegistry.GetAll().Count() < 2)
-        {
-        }
-        
-        // assert the state was saved
-        Mock.Get(state).Verify(_ => _.WriteStateAsync());
-        Assert.That(state.State.PendingImageGenerationRequests, Has.Count.EqualTo(1));
-    }
-
-    [Test]
-    public async Task Test1()
-    {
-        var state = new SchedulerState();
-        
-        var mockSchedulerState = new Mock<IPersistentState<SchedulerState>>();
-        mockSchedulerState.SetupGet(o => o.State).Returns(state);
-        
-        /*
-        var mockMapper = new Mock<IAttributeToFactoryMapper<PersistentStateAttribute>>();
-        mockMapper.Setup(o => o.GetFactory(It.IsAny<ParameterInfo>(), It.IsAny<PersistentStateAttribute>())).Returns(context => mockSchedulerState.Object);
-
-        silo.AddService(mockMapper.Object);*/
-        
         var builder = new TestClusterBuilder();
         var cluster = builder.AddSiloBuilderConfigurator<SiloConfigurator>().Build();
         cluster.Deploy();
@@ -89,19 +50,138 @@ public class ScheduleGrainTest
         
         var now = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
         await scheduler.AddImageGenerationRequest("myRequestId", "myAccountInfo", now);
+        
+        await Tick();
 
-        Assert.Equals(1, scheduler.GetStartedImageGenerationRequestsAsync().Result.Count);
+        Assert.That(scheduler.GetPendingImageGenerationRequestsAsync().Result, Has.Count.EqualTo(1));
         
         cluster.StopAllSilos();
     }
+    
+    [Test]
+    public async Task Scheduler_FailRequest_Test()
+    {
+        var builder = new TestClusterBuilder();
+        var cluster = builder.AddSiloBuilderConfigurator<SiloConfigurator>().Build();
+        cluster.Deploy();
+
+        var scheduler = cluster.GrainFactory.GetGrain<ISchedulerGrain>("SchedulerGrain");
+        
+        var now = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
+        await scheduler.AddImageGenerationRequest("myRequestId", "myAccountInfo", now);
+        
+        await Tick();
+
+        await scheduler.ReportFailedImageGenerationRequestAsync(new RequestStatus
+        {
+            Message = "failed",
+            RequestId = "myRequestId",
+            Status = RequestStatusEnum.Failed
+        });
+        
+        Assert.That(scheduler.GetFailedImageGenerationRequestsAsync().Result, Has.Count.EqualTo(1));
+        
+        cluster.StopAllSilos();
+    }
+    
+    [Test]
+    public async Task Scheduler_FailRequest_Reprocessed_Test()
+    {
+        var builder = new TestClusterBuilder();
+        var cluster = builder.AddSiloBuilderConfigurator<SiloConfigurator>().Build();
+        cluster.Deploy();
+
+        var scheduler = cluster.GrainFactory.GetGrain<ISchedulerGrain>("SchedulerGrain");
+        
+        var now = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
+        await scheduler.AddImageGenerationRequest("myRequestId", "myAccountInfo", now);
+        
+        await Tick();
+
+        await scheduler.ReportFailedImageGenerationRequestAsync(new RequestStatus
+        {
+            Message = "failed",
+            RequestId = "myRequestId",
+            Status = RequestStatusEnum.Failed
+        });
+        
+        await Tick();
+        
+        Assert.That(scheduler.GetPendingImageGenerationRequestsAsync().Result, Has.Count.EqualTo(1));
+        
+        cluster.StopAllSilos();
+    }
+    
+    [Test]
+    public async Task Scheduler_CompletedRequest_Test()
+    {
+        var builder = new TestClusterBuilder();
+        var cluster = builder.AddSiloBuilderConfigurator<SiloConfigurator>().Build();
+        cluster.Deploy();
+
+        var scheduler = cluster.GrainFactory.GetGrain<ISchedulerGrain>("SchedulerGrain");
+        
+        var now = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
+        await scheduler.AddImageGenerationRequest("myRequestId", "myAccountInfo", now);
+        
+        await Tick();
+
+        await scheduler.ReportCompletedImageGenerationRequestAsync(new RequestStatus
+        {
+            Message = "Completed",
+            RequestId = "myRequestId",
+            Status = RequestStatusEnum.Completed
+        });
+        
+        Assert.That(scheduler.GetPendingImageGenerationRequestsAsync().Result, Has.Count.EqualTo(0));
+        Assert.That(scheduler.GetFailedImageGenerationRequestsAsync().Result, Has.Count.EqualTo(0));
+        Assert.That(scheduler.GetStartedImageGenerationRequestsAsync().Result, Has.Count.EqualTo(0));
+        
+        cluster.StopAllSilos();
+    }
+
+    private static async Task Tick()
+    {
+        var timerEntries = FakeTimerRegistry.GetAll();
+        foreach (var fakeTimerEntry in timerEntries)
+        {
+            await fakeTimerEntry.TickAsync();
+        }
+    }
+
+    public static FakeTimerRegistry FakeTimerRegistry;
     
     private class SiloConfigurator : ISiloConfigurator
     {
         public void Configure(ISiloBuilder siloBuilder) => siloBuilder
             .AddMemoryGrainStorage(Constants.MySqlSchrodingerImageStore)
-            .ConfigureServices(static services =>
+            .ConfigureServices(services =>
             {
-                services.AddSingleton<FakeTimerRegistry>();
+                var state = new SchedulerState();
+                
+                state.ApiAccountInfoList = new List<APIAccountInfo>
+                {
+                    new APIAccountInfo
+                    {
+                        ApiKey = "apiKey1",
+                        Description = "mocked api key 1",
+                        Email = "mock@mock.com",
+                        Tier = 0,
+                        MaxQuota = 5
+                    }
+                };
+                
+        
+                var mockSchedulerState = new Mock<IPersistentState<SchedulerState>>();
+                mockSchedulerState.SetupGet(o => o.State).Returns(state);
+                
+                var mockMapper = new Mock<IAttributeToFactoryMapper<PersistentStateAttribute>>();
+                mockMapper.Setup(o => o.GetFactory(It.IsAny<ParameterInfo>(), It.IsAny<PersistentStateAttribute>())).Returns(context => mockSchedulerState.Object);
+                
+                FakeTimerRegistry = new FakeTimerRegistry();
+                
+                services.AddSingleton(FakeTimerRegistry);
+                services.AddSingleton(mockMapper.Object);
                 services.AddSingleton<ITimerRegistry>(_ => _.GetService<FakeTimerRegistry>());
             });
     }
