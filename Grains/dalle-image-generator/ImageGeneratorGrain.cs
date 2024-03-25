9cf28438942456ce29e83890c0a48f03d3b23c84
ymbrucesgr <ymbruce@schrodingernft.ai>
2024-03-25 14:08:21 +0800

    fix: remove useless logs

--- Grains/dalle-image-generator/ImageGeneratorGrain.cs
+++ Grains/dalle-image-generator/ImageGeneratorGrain.cs
@@ -347,7 +347,7 @@ public class ImageGeneratorGrain : Grain, IImageGeneratorGrain, IDisposable
             throw new DalleException(DalleErrorCode.api_call_failed, e.Message);
         }
         
-        _logger.LogError($"Dalle ImageGeneration ResponseCode : {response.StatusCode}");
+        _logger.LogInformation($"Dalle ImageGeneration ResponseCode : {response.StatusCode}");
         
         if(dalleResponse.Error != null)
         {
